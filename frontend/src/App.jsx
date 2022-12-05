b03311de85d17f4a6877b5aa1eb3ea52e5a9e51a
Joao Marcelo Alves <joao.alves@liferay.com>
2022-12-04 21:20:17 -0300

    Create React and Nodejs app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+const port = "http://localhost:5000";
+function App() {
+  const [username, setUsername] = useState("");
+
+  const getNames = async () => {
+    const response = await axios.get(`${port}/names`);
+    setUsername(response.data);
+  };
+
+  useEffect(() => {
+    getNames();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>My FrontEnd</h1>
+      <h3>My name is {username}</h3>
+    </div>
+  );
+}
+
+export default App;
